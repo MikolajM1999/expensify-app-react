3bf6426c9c8ba686c99da080003eea170c190ecd
Mikołaj Malicki <mikolajmalicki000@gmail.com>
2019-01-26 01:51:18 +0100

    Updated ExpensesSummary component

--- src/components/ExpensesSummary.js
+++ src/components/ExpensesSummary.js
@@ -4,15 +4,17 @@ import selectExpenses from '../selectors/expenses'
 import selectExpensesTotal from '../selectors/expenses-total'
 import numeral from 'numeral'
 
-export const ExpensesSummary = ({ expenses }) => (
+export const ExpensesSummary = ({ expenses, expensesCount }) => (
     <div>
-        <h1>{`Vieving ${expenses.length} ${expenses.length === 1 ? 'expense' : 'expenses'} totalling ${numeral(selectExpensesTotal(expenses) / 100).format('$0,0.00')}`}</h1>
+        <h1>{`Vieving ${expenses.length} ${expenses.length === 1 ? 'expense' : 'expenses'} totalling ${numeral(expensesCount / 100).format('$0,0.00')}`}</h1>
     </div>
 )
 
 const mapStateToProps = (state) => {
+    const visibleExpenses = selectExpenses(state.expenses, state.filters)
     return {
-        expenses: selectExpenses(state.expenses, state.filters)
+        expenses: visibleExpenses,
+        expensesCount: selectExpensesTotal(visibleExpenses)
     };
 }
 
--- src/tests/components/ExpensesSummary.test.js
+++ src/tests/components/ExpensesSummary.test.js
@@ -9,11 +9,11 @@ test('should render ExpensesSummary correctly with 0 expenses', () => {
 })
 
 test('should render ExpensesSummary correctly with 1 expense', () => {
-    const wrapper = shallow(<ExpensesSummary expenses={[expenses[0]]} />)
+    const wrapper = shallow(<ExpensesSummary expenses={[expenses[0]]} expensesCount={123} />)
     expect(wrapper).toMatchSnapshot()
 })
 
 test('should render ExpensesSummary correctly with multiple expenses', () => {
-    const wrapper = shallow(<ExpensesSummary expenses={expenses} />)
+    const wrapper = shallow(<ExpensesSummary expenses={expenses} expensesCount={11122233344} />)
     expect(wrapper).toMatchSnapshot()
 })
